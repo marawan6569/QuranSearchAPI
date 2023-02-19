e64e36e0fbb06ab28848db7ce7316860221cd4bb
marwan <marawan6569@gmail.com>
2023-02-19 21:32:39 +0200

    add .env file and  update urls.py

--- search/urls.py
+++ search/urls.py
@@ -1,20 +1,20 @@
 from django.urls import path
 from .views import *
 from .api_views import *
+
 app_name = 'search'
 
 urlpatterns = [
-    path('', viweAll, name='viweAll'),
-    path('search', searchByWords, name='searchByWords'),
+    path('', api_docs, name='viweAll'),
+    path('search/', searchByWords, name='searchByWords'),
     path('surah/<int:id>/', getSurah, name='getSurah'),
     ################## API ##########################
-    path('api', api_docs, name='apiDocs'),
-    path('api/allVerses', all_verses_api, name='versesListAPI'),
-    path('api/search/<str:keyWords>', search_api, name='searchAPI'),
-    path('api/getSurah/<int:id>', get_surah_api, name='getSurahAPI'),
-    path('api/getVerse/<int:surahId>/<int:verseNum>', get_verse_api, name='getVerseAPI'),
-    path('api/getRangeOfVerses/<int:start>/<int:end>', get_range_of_verses_api, name='getRangeOfVerses'),
-    path('api/magicSearch/verseStartsWith/<str:letters>', verse_starts_with, name='verseStartsWithAPI'),
-    path('api/magicSearch/verseEndsWith/<str:letters>', verse_ends_with, name='verseEndsWithAPI'),
-
+    path('api/', api_docs, name='apiDocs'),
+    path('api/allVerses/', all_verses_api, name='versesListAPI'),
+    path('api/search/<str:keyWords>/', search_api, name='searchAPI'),
+    path('api/getSurah/<int:id>/', get_surah_api, name='getSurahAPI'),
+    path('api/getVerse/<int:surahId>/<int:verseNum>/', get_verse_api, name='getVerseAPI'),
+    path('api/getRangeOfVerses/<int:start>/<int:end>/', get_range_of_verses_api, name='getRangeOfVerses'),
+    path('api/magicSearch/verseStartsWith/<str:letters>/', verse_starts_with, name='verseStartsWithAPI'),
+    path('api/magicSearch/verseEndsWith/<str:letters>/', verse_ends_with, name='verseEndsWithAPI'),
 ]
